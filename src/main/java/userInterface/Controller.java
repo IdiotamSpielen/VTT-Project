a7c6fc479b93b8c313a452821eb3ed620a7b51b1
IdiotamSpielen <IdiotamSpielen@gmail.com>
2023-07-20 21:06:52 +0200

    I managed to place a rectangle in the upper left corner. It's really scuffed, but it works. Now if only I could get it to resize properly...

--- src/main/java/userInterface/Controller.java
+++ src/main/java/userInterface/Controller.java
@@ -8,18 +8,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import handlers.FeedbackHandler;
-import creators.SpellCreator;
 
 public class Controller {
 
     private FeedbackHandler feedbackHandler;
     @FXML
     private Text feedbackText;
-    private SpellCreator spellCreator;
     @FXML
     private Label label;
 
@@ -37,14 +34,23 @@ public class Controller {
 
 
     public void initialize() {
-
-    tableTop.prefWidthProperty().bind(rootPane.widthProperty().multiply(0.8));
-    tableTop.prefHeightProperty().bind(rootPane.heightProperty().multiply(0.8));
+    tableTop = new TableTop();
+    tableTopPane.getChildren().add(tableTop);
+    double minWidth = 400;
+    double minHeight = 400;
+    tableTop.setMinWidth(minWidth);
+    tableTop.setMinHeight(minHeight);
+    tableTop.prefWidthProperty().bind(rootPane.widthProperty().multiply(0.765));
+    tableTop.prefHeightProperty().bind(rootPane.heightProperty().multiply(0.77));
 
     ChangeListener<Number> sizeChangeListener = (observable, oldValue, newValue) -> {
-        double tableTopWidth = rootPane.getWidth() * 0.8;
-        double tableTopHeight = rootPane.getHeight() * 0.8;
+        double tableTopWidth = rootPane.getWidth() * 0.765;
+        double tableTopHeight = rootPane.getHeight() * 0.77;
         tableTop.setDimensions(tableTopWidth, tableTopHeight);
+    };
+
+    rootPane.widthProperty().addListener(sizeChangeListener);
+    rootPane.heightProperty().addListener(sizeChangeListener);
 
     createSpellButton.setOnAction(event -> {
         // Handle button click event
@@ -67,6 +73,5 @@ public class Controller {
         BorderPane.setAlignment(feedbackText, Pos.CENTER);
         layoutPane.setCenter(feedbackText);
         });
-    };
     }
 }
