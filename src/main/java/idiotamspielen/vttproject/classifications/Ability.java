ff364b5ee04280d1938d2ea6f5c6069bea9433b9
IdiotamSpielen <IdiotamSpielen@gmail.com>
2024-08-09 13:59:57 +0200

    Refactor Character class and add Ability enum
    Revised Character class to use final fields and added a constructor. Replaced individual ability scores with an EnumMap for better clarity and maintenance. Added the Ability enum to clearly represent different abilities and their indices.

--- src/main/java/idiotamspielen/vttproject/classifications/Ability.java
+++ src/main/java/idiotamspielen/vttproject/classifications/Ability.java
@@ -1,6 +1,21 @@
 package idiotamspielen.vttproject.classifications;
 
-public class Ability{
-    private String name;
-    private String description;
+// Enum für Fähigkeiten, um Indices klarer darzustellen
+public enum Ability {
+    STRENGTH(0),
+    DEXTERITY(1),
+    CONSTITUTION(2),
+    INTELLIGENCE(3),
+    WISDOM(4),
+    CHARISMA(5);
+
+    private final int index;
+
+    Ability(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
--- src/main/java/idiotamspielen/vttproject/classifications/Character.java
+++ src/main/java/idiotamspielen/vttproject/classifications/Character.java
@@ -1,155 +1,93 @@
 package idiotamspielen.vttproject.classifications;
 
+import java.util.Map;
+
 public class Character{
-    private String name;
-    private String backstory;
-    private String bonds;
-    private String flaws;
-    private String alignment;
-    private int initiative;
-    private int armorClass;
-    private int movementSpeed;
-    private int hitPoints;
-    private int strength;
-    private int dexterity;
-    private int constitution;
-    private int intelligence;
-    private int wisdom;
-    private int charisma;
+    private final String name;
+    private final String backstory;
+    private final String bonds;
+    private final String flaws;
+    private final String alignment;
+    private final int initiative;
+    private final int armorClass;
+    private final int movementSpeed;
+    private final int hitPoints;
+    // EnumMap for abilities
+    private final Map<Ability, Integer> abilityScores;
 
-    private CharacterRace characterRace;
-    private CharacterClass characterClass;
+    // References to race and class
+    private final CharacterRace characterRace;
+    private final CharacterClass characterClass;
 
-    public String getName() {
-        return name;
+    //Constructor
+    public Character(String name, String backstory, String bonds, String flaws, String alignment, int initiative,
+                     int armorClass, int movementSpeed, int hitPoints, CharacterRace characterRace,
+                     CharacterClass characterClass, Map<Ability, Integer> abilityScores) {
+        this.name = name;
+        this.backstory = backstory;
+        this.bonds = bonds;
+        this.flaws = flaws;
+        this.alignment = alignment;
+        this.initiative = initiative;
+        this.armorClass = armorClass;
+        this.movementSpeed = movementSpeed;
+        this.hitPoints = hitPoints;
+        this.characterRace = characterRace;
+        this.characterClass = characterClass;
+        this.abilityScores = abilityScores;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    // getting ability scores
+    public int getAbilityScore(Ability ability) {
+        return abilityScores.getOrDefault(ability, 0);
+    }
+
+    // calculating modifiers
+    public int getAbilityModifier(Ability ability) {
+        return (getAbilityScore(ability) - 10) / 2;
+    }
+
+    public String getName() {
+            return name != null ? name : "Unknown character";
     }
 
     public int getInitiative() {
         return initiative;
     }
 
-    public void setInitiative(int initiative) {
-        this.initiative = initiative;
-    }
-
     public int getArmorClass() {
         return armorClass;
-    }
-
-    public void setArmorClass(int armorClass) {
-        this.armorClass = armorClass;
     }
 
     public int getMovementSpeed() {
         return movementSpeed;
     }
 
-    public void setMovementSpeed(int movementSpeed) {
-        this.movementSpeed = movementSpeed;
-    }
-
     public int getHitPoints() {
         return hitPoints;
-    }
-
-    public void setHitPoints(int hitPoints) {
-        this.hitPoints = hitPoints;
-    }
-
-    public void setStrength(int strength) {this.strength = strength;}
-
-    public void setDexterity(int dexterity) {
-        this.dexterity = dexterity;
-    }
-
-    public void setConstitution(int constitution) {
-        this.constitution = constitution;
-    }
-
-    public void setIntelligence(int intelligence) {
-        this.intelligence = intelligence;
-    }
-
-    public void setWisdom(int wisdom) {
-        this.wisdom = wisdom;
-    }
-
-    public void setCharisma(int charisma) {
-        this.charisma = charisma;
     }
 
     public String getBackstory() {
         return backstory;
     }
 
-    public void setBackstory(String backstory) {
-        this.backstory = backstory;
-    }
-
     public String getBonds() {
         return bonds;
-    }
-
-    public void setBonds(String bonds) {
-        this.bonds = bonds;
     }
 
     public String getFlaws() {
         return flaws;
     }
 
-    public void setFlaws(String flaws) {
-        this.flaws = flaws;
-    }
-
     public String getAlignment() {
         return alignment;
-    }
-
-    public void setAlignment(String alignment) {
-        this.alignment = alignment;
     }
 
     public CharacterRace getRace() {
         return characterRace;
     }
 
-    public void setRace(CharacterRace race) {
-        this.characterRace = race;
-    }
-
     public CharacterClass getCharacterClass() {
         return characterClass;
-    }
-
-    public void setCharacterClass(CharacterClass dndClass) {
-        this.characterClass = dndClass;
-    }
-
-    public int getAbilityScore(int abilityIndex) {
-        switch (abilityIndex) {
-            case 0:
-                return strength;
-            case 1:
-                return dexterity;
-            case 2:
-                return constitution;
-            case 3:
-                return intelligence;
-            case 4:
-                return wisdom;
-            case 5:
-                return charisma;
-            default:
-                throw new IllegalArgumentException("Invalid ability index: " + abilityIndex);
-        }
-    }
-
-    public int getAbilityModifier(int abilityIndex) {
-        return (getAbilityScore(abilityIndex) - 10) / 2;
     }
 }
