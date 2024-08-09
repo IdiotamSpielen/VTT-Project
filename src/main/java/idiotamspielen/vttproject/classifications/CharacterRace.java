c3d262a550d68319c3fe28bdd0dd35067a9992ae
IdiotamSpielen <IdiotamSpielen@gmail.com>
2024-08-09 13:58:53 +0200

    Add RacialTrait management to CharacterRace
    Introduced methods to manage RacialTraits within CharacterRace, including add, remove, and getter/setter methods. Enhanced RacialTrait with the Feature interface and added appropriate getters, setters, and a toString method.

--- src/main/java/idiotamspielen/vttproject/classifications/CharacterRace.java
+++ src/main/java/idiotamspielen/vttproject/classifications/CharacterRace.java
@@ -1,18 +1,80 @@
 package idiotamspielen.vttproject.classifications;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CharacterRace {
 //The race gives the character an armor class, a list of racial traits, a base weapon, Weapon and/or spell proficiencies, and potentially a subrace
     protected String name;
     protected String description;
     protected int armorClass;
+    protected List<RacialTrait> racialTraits;
 
 
     public CharacterRace(String name, String description) {
         this.name = name;
         this.description = description;
+        this.racialTraits = new ArrayList<>(); // Initialize the list of racial traits
     }
 
+    // Method to add a racial trait
+    public void addRacialTrait(RacialTrait trait) {
+        racialTraits.add(trait);
+    }
+
+    // Method to remove a racial trait
+    public void removeRacialTrait(RacialTrait trait) {
+        racialTraits.remove(trait);
+    }
+
+    // Getter for the list of racial traits
+    public List<RacialTrait> getRacialTraits() {
+        return racialTraits;
+    }
+
+    // Setter for the list of racial traits
+    public void setRacialTraits(List<RacialTrait> racialTraits) {
+        this.racialTraits = racialTraits;
+    }
+
+    // Getter for name
     public String getName() {
         return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for description
+    public String getDescription() {
+        return description;
+    }
+
+    // Setter for description
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Getter for armor class
+    public int getArmorClass() {
+        return armorClass;
+    }
+
+    // Setter for armor class
+    public void setArmorClass(int armorClass) {
+        this.armorClass = armorClass;
+    }
+
+    // Override toString for better representation of CharacterRace
+    @Override
+    public String toString() {
+        return "CharacterRace{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", armorClass=" + armorClass +
+                ", racialTraits=" + racialTraits +
+                '}';
     }
 }
--- src/main/java/idiotamspielen/vttproject/classifications/RacialTrait.java
+++ src/main/java/idiotamspielen/vttproject/classifications/RacialTrait.java
@@ -1,4 +1,37 @@
 package idiotamspielen.vttproject.classifications;
 
-public class RacialTrait {
+public class RacialTrait implements Feature {
+    private String name;
+    private String description;
+
+    public RacialTrait(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "RacialTrait{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
