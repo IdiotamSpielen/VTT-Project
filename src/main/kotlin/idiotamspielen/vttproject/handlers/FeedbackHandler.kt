08a5711dd42c47f445c132b371d265c4b8759fb8
IdiotamSpielen <IdiotamSpielen@gmail.com>
2024-11-20 22:08:51 +0100

    Convert FeedbackHandler from Java to Kotlin
    Rewrote the FeedbackHandler class from Java to Kotlin. This change includes the refactoring of method names and some adjustments in handling transitions and bindings for feedback text status updates.

--- src/main/kotlin/idiotamspielen/vttproject/handlers/FeedbackHandler.kt
+++ src/main/kotlin/idiotamspielen/vttproject/handlers/FeedbackHandler.kt
@@ -1,44 +1,44 @@
-package idiotamspielen.vttproject.handlers;
+package idiotamspielen.vttproject.handlers
 
-import javafx.animation.FadeTransition;
-import javafx.animation.PauseTransition;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
+import javafx.animation.FadeTransition
+import javafx.animation.PauseTransition
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.scene.paint.Color
+import javafx.scene.text.Text
+import javafx.util.Duration
 
-public class FeedbackHandler {
-    private final Text feedbackText;
+class FeedbackHandler(private val feedbackText: Text) {
 
-    public FeedbackHandler(Text feedbackText) {
-        this.feedbackText = feedbackText;
+    fun saveSuccess(spellName: String?) {
+        feedbackText.opacity = 1.0
+        feedbackText.text = "Spell saved as $spellName"
+        feedbackText.fill = Color.GREEN
+        fadeOutFeedbackText()
     }
 
-    public void displaySuccess(String spellName) {
-        feedbackText.setText("Spell saved as " + spellName);
-        feedbackText.setFill(Color.GREEN);
-        fadeOutFeedbackText();
+    fun saveError() {
+        feedbackText.opacity = 1.0
+        feedbackText.text = "Failed to save Spell. Check log for further information."
+        feedbackText.fill = Color.RED
+        fadeOutFeedbackText()
     }
 
-    public void displayError() {
-        feedbackText.setText("Failed to save Spell. Check log for further information.");
-        feedbackText.setFill(Color.RED);
-        fadeOutFeedbackText();
+    fun userdataError() {
+        feedbackText.opacity = 1.0
+        feedbackText.text = "Failed to save Spell. Check your inputs and try again."
+        feedbackText.fill = Color.RED
+        fadeOutFeedbackText()
     }
 
-    public void pointOutIncompetence() {
-        feedbackText.setText("Failed to save Spell. Check your inputs and try again.");
-        feedbackText.setFill(Color.RED);
-        fadeOutFeedbackText();
-    }
+    private fun fadeOutFeedbackText() {
+        val fadeTransition = FadeTransition(Duration.seconds(1.0), feedbackText)
+        fadeTransition.fromValue = 1.0
+        fadeTransition.toValue = 0.0
 
-    private void fadeOutFeedbackText() {
-        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), feedbackText);
-        fadeTransition.setFromValue(1.0);
-        fadeTransition.setToValue(0.0);
+        val pauseTransition = PauseTransition(Duration.seconds(1.5))
+        pauseTransition.onFinished = EventHandler { event: ActionEvent? -> fadeTransition.play() }
 
-        PauseTransition pauseTransition = new PauseTransition(Duration.seconds(1.5));
-        pauseTransition.setOnFinished(event -> fadeTransition.play());
-
-        pauseTransition.play();
+        pauseTransition.play()
     }
 }
