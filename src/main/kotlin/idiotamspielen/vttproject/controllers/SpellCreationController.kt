e69fdfe0febc15dbf1c4587728e86bd70e2d27ac
IdiotamSpielen <IdiotamSpielen@gmail.com>
2024-11-21 20:01:32 +0100

    Refactor spell creation to include validation checks
    Refactored the createSpell() function to validate the spell and provide specific exceptions in case of failure. Introduced InvalidSpellException and SpellNotSavedException to handle and inform of issues during spell creation.

--- src/main/kotlin/idiotamspielen/vttproject/controllers/SpellCreationController.kt
+++ src/main/kotlin/idiotamspielen/vttproject/controllers/SpellCreationController.kt
@@ -24,7 +24,7 @@ class SpellCreationController : Controller() {
     val isRitual = SimpleBooleanProperty(false)
     val isConcentration = SimpleBooleanProperty(false)
 
-    fun createSpell(): Boolean {
+    fun createSpell(){
         val newSpellCreator = SpellCreatorV2(FileHandlerV2(SpellV2::class.java, "spells"))
 
         newSpellCreator.create(
@@ -41,10 +41,17 @@ class SpellCreationController : Controller() {
             concentration = isConcentration.get()
         )
 
-        return newSpellCreator.isSpellSaved()
+        if (!newSpellCreator.isSpellValid()) {
+            throw InvalidSpellException("The Spell is invalid. Check your input.")
+        } else if (!newSpellCreator.isSpellSaved()) {
+            throw SpellNotSavedException("The Spell was not saved. Check the logs for more information.")
+        }
     }
 
     private fun SimpleStringProperty.getOrElse(default : String): String{
         return this.get() ?: default
     }
 }
+
+class InvalidSpellException(message: String) : Exception(message)
+class SpellNotSavedException(message: String) : Exception(message)
