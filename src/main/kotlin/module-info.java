945502e78108fc2bcd70b0065e36024b214b0069
IdiotamSpielen <IdiotamSpielen@gmail.com>
2024-11-28 20:24:03 +0100

    Update module-info.java exports and opens statements
    Removed exports and opens for the 'creators' package and added 'tornadofx' to the opens statement for the 'classifications' package.

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -9,15 +9,13 @@ module idiotamspielen.vttproject{
         exports idiotamspielen.vttproject to javafx.graphics, tornadofx;
         exports idiotamspielen.vttproject.controllers to javafx.fxml, tornadofx;
         exports idiotamspielen.vttproject.handlers to javafx.fxml, tornadofx;
-        exports idiotamspielen.vttproject.creators to javafx.fxml, tornadofx;
         exports idiotamspielen.vttproject.classifications to javafx.fxml, com.fasterxml.jackson.databind, tornadofx;
         exports idiotamspielen.vttproject.userInterface to javafx.fxml, tornadofx;
         exports idiotamspielen.vttproject.views to javafx.fxml, tornadofx;
         opens idiotamspielen.vttproject to javafx.fxml, tornadofx;
         opens idiotamspielen.vttproject.controllers to javafx.fxml, tornadofx;
         opens idiotamspielen.vttproject.handlers to javafx.fxml, tornadofx;
-        opens idiotamspielen.vttproject.creators to javafx.fxml, tornadofx;
-        opens idiotamspielen.vttproject.classifications to javafx.fxml, com.fasterxml.jackson.databind;
+        opens idiotamspielen.vttproject.classifications to javafx.fxml, com.fasterxml.jackson.databind, tornadofx;
         opens idiotamspielen.vttproject.userInterface to javafx.fxml, tornadofx;
         opens idiotamspielen.vttproject.views to javafx.fxml, tornadofx;
 }
