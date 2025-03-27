d42984f2179a3a3f6e2b2efc19f90c8cca44703c
IdiotamSpielen <IdiotamSpielen@gmail.com>
2025-03-27 18:50:41 +0100

    Remove unused Subclass file from the project.
    The Subclass file was deemed unnecessary and has been deleted to simplify the codebase. This change helps reduce clutter and ensures that only relevant files are maintained.

--- src/main/kotlin/idiotamspielen/vttproject/classifications/Subclass.kt
+++
@@ -1,4 +0,0 @@
-package idiotamspielen.vttproject.classifications
-
-class Subclass(name: String?, description: String?, abilities: MutableList<*>?) :
-    CharacterClass(name, description, abilities)
