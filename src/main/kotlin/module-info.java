b40b7f590c75ac7c4037e335be8380fc0845ff37
IdiotamSpielen <IdiotamSpielen@gmail.com>
2025-03-27 16:21:43 +0100

    Reformat module-info.java for consistent indentation
    Reordered and properly aligned "requires," "exports," and "opens" directives to maintain consistent formatting. This change improves readability and adheres to standard code style conventions.

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -1,22 +1,22 @@
 module idiotamspielen.vttproject{
-        requires javafx.controls;
-        requires javafx.fxml;
-        requires com.fasterxml.jackson.databind;
-        requires org.jetbrains.annotations;
-        requires kotlin.stdlib;
-        requires tornadofx;
-        requires org.slf4j;
+    requires kotlin.stdlib;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires org.jetbrains.annotations;
+    requires tornadofx;
+    requires org.slf4j;
 
-        exports idiotamspielen.vttproject to javafx.graphics, tornadofx, org.slf4j;
-        exports idiotamspielen.vttproject.controllers to javafx.fxml, tornadofx, org.slf4j;
-        exports idiotamspielen.vttproject.handlers to javafx.fxml, tornadofx, org.slf4j;
-        exports idiotamspielen.vttproject.classifications to javafx.fxml, com.fasterxml.jackson.databind, tornadofx, org.slf4j;
-        exports idiotamspielen.vttproject.userInterface to javafx.fxml, tornadofx , org.slf4j;
-        exports idiotamspielen.vttproject.views to javafx.fxml, tornadofx , org.slf4j;
-        opens idiotamspielen.vttproject to javafx.fxml, tornadofx, org.slf4j;
-        opens idiotamspielen.vttproject.controllers to javafx.fxml, tornadofx, org.slf4j;
-        opens idiotamspielen.vttproject.handlers to javafx.fxml, tornadofx, org.slf4j;
-        opens idiotamspielen.vttproject.classifications to javafx.fxml, com.fasterxml.jackson.databind, tornadofx, org.slf4j;
-        opens idiotamspielen.vttproject.userInterface to javafx.fxml, tornadofx, org.slf4j;
-        opens idiotamspielen.vttproject.views to javafx.fxml, tornadofx, org.slf4j;
+    exports idiotamspielen.vttproject to javafx.graphics, tornadofx, org.slf4j;
+    exports idiotamspielen.vttproject.controllers to javafx.fxml, tornadofx, org.slf4j;
+    exports idiotamspielen.vttproject.handlers to javafx.fxml, tornadofx, org.slf4j;
+    exports idiotamspielen.vttproject.classifications to javafx.fxml, com.fasterxml.jackson.databind, tornadofx, org.slf4j;
+    exports idiotamspielen.vttproject.userInterface to javafx.fxml, tornadofx , org.slf4j;
+    exports idiotamspielen.vttproject.views to javafx.fxml, tornadofx, org.slf4j;
+    opens idiotamspielen.vttproject to javafx.fxml, tornadofx, org.slf4j;
+    opens idiotamspielen.vttproject.controllers to javafx.fxml, tornadofx, org.slf4j;
+    opens idiotamspielen.vttproject.handlers to javafx.fxml, tornadofx, org.slf4j;
+    opens idiotamspielen.vttproject.classifications to javafx.fxml, com.fasterxml.jackson.databind, tornadofx, org.slf4j;
+    opens idiotamspielen.vttproject.userInterface to javafx.fxml, tornadofx, org.slf4j;
+    opens idiotamspielen.vttproject.views to javafx.fxml, tornadofx, org.slf4j;
 }
