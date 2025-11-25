6189da2f3a0ebdf730785a00daa5b7e9429a2f30
IdiotamSpielen <IdiotamSpielen@gmail.com>
2025-11-25 20:48:01 +0100

    Remove module system configuration and adjust build setup.
    The `module-info.java` file was deleted, and Java module configurations were removed from `build.gradle.kts`. Updated dependencies, simplified build scripts, and improved test logging settings for better maintainability and compatibility.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     kotlin("jvm") version "2.2.21" // Kotlin Plugin
     id("org.openjfx.javafxplugin") version "0.1.0" // JavaFX Plugin
-    id("com.gradleup.shadow") version "8.3.8" //Shadow Plugin
-    id("edu.sc.seis.launch4j") version "3.0.0"
+    id("com.gradleup.shadow") version "8.3.9"
     application
 }
 
@@ -25,28 +24,35 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
     implementation("org.webjars.npm:types__filewriter:0.0.29")
     implementation("org.jetbrains:annotations:26.0.2")
-    implementation("org.slf4j:slf4j-api:2.0.17")
-    implementation("edu.sc.seis.launch4j:launch4j:3.0.0")
-    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("ch.qos.logback:logback-classic:1.5.21")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
+    testImplementation("org.junit.platform:junit-platform-commons:1.13.4")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.13.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.4")
+
+    runtimeOnly("org.openjfx:javafx-controls:24-ea+19:win")
+    runtimeOnly("org.openjfx:javafx-fxml:24-ea+19:win")
+    runtimeOnly("org.openjfx:javafx-graphics:24-ea+19:win")
+    runtimeOnly("org.openjfx:javafx-base:24-ea+19:win")
+    runtimeOnly("org.slf4j:slf4j-api:2.0.17")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.21")
 }
 
 sourceSets {
     main {
-        java.srcDirs("src/main/java")
         kotlin.srcDirs("src/main/kotlin")
     }
     test {
-        java.srcDirs("src/test/java") // Testquellen für Java
-        kotlin.srcDirs("src/test/kotlin") // Testquellen für Kotlin
+        kotlin.srcDirs("src/test/kotlin")
     }
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
-    modularity.inferModulePath.set(true)
+    modularity.inferModulePath.set(false)
 }
 
 javafx {
@@ -54,27 +60,17 @@ javafx {
     modules("javafx.controls", "javafx.fxml")
 }
 
-launch4j {
-    mainClassName = "idiotamspielen.vttproject.MainKt"
-    jarTask = tasks.shadowJar.get()
-    outfile = "build/VTT.exe"
-}
-
 application {
     mainClass.set("idiotamspielen.vttproject.MainKt") // Hauptklasse
 }
 
-tasks {
-    shadowJar {
-        archiveBaseName.set("VTT")
-        archiveClassifier.set("")
-        archiveVersion.set(project.version.toString())
-        mergeServiceFiles()
-    }
-}
-
 tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed") // Zeigt Details zu den Tests
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL // Stacktraces anzeigen
+        showStandardStreams = true // Standard-Ausgaben anzeigen
+    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
