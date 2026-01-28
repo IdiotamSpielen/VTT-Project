43fdb91ec3c28cc3dd969b0df666f6f08452419a
Max Kuhn <max.kuhn@koenig-bauer.com>
2026-01-28 12:37:35 +0100

    Dependency Overhaul

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,18 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
 }
 
-val appVersion = System.getenv("GITHUB_REF_NAME") ?: "1.0.0"
+group = "com.idiotamspielen" // Optional: Dein Group ID
+version = System.getenv("GITHUB_REF_NAME")?.removePrefix("v") ?: "1.0.0"
 
 kotlin {
     jvmToolchain(21)
 }
 
 // setting up a mockito agent for testing
-val mockitoAgent: Configuration by configurations.creating
+//val mockitoAgent: Configuration by configurations.creating
 
 dependencies {
-    // Compose Desktop Dependencies
+    // --- Compose Desktop ---
     implementation(compose.desktop.currentOs)
     implementation("org.jetbrains.compose.material:material:1.10.0")
     implementation("org.jetbrains.compose.ui:ui:1.10.0")
@@ -25,24 +26,26 @@ dependencies {
     implementation("org.jetbrains.compose.material:material-icons-extended:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.10.1")
 
-    // libraries for working with a database
+    // --- Database (SQLite + Exposed) ---
     implementation("org.jetbrains.exposed:exposed-core:1.0.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:1.0.0")
     implementation("org.jetbrains.exposed:exposed-dao:1.0.0")
     implementation("org.xerial:sqlite-jdbc:3.49.1.0")
 
-    // logic libraries (might become obsolete in this change)
-    implementation("org.jetbrains:annotations:26.0.2")
+    // --- Logging ---
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.5.+")
 
-    // Testing
+    // --- Testing ---
+    // JUnit
     testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
-    testImplementation("org.mockito:mockito-core:5.+")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
 
-    mockitoAgent("org.mockito:mockito-core:5.21.0") { isTransitive = false }
+    //Mockito
+    testImplementation("org.mockito:mockito-core:5.+")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.+")
+    //mockitoAgent("org.mockito:mockito-core:5.+") { isTransitive = false }
 }
 
 tasks.withType<KotlinCompile>().configureEach {
@@ -54,10 +57,10 @@ tasks.withType<KotlinCompile>().configureEach {
 tasks.withType<Test> {
     useJUnitPlatform()
     // Mockito Agent einbinden
-    val agentFile = mockitoAgent.find { it.name.startsWith("mockito-core") }
-    if (agentFile != null) {
-        jvmArgs("-javaagent:${agentFile.absolutePath}")
-    }
+    //val agentFile = mockitoAgent.find { it.name.startsWith("mockito-core") }
+    //if (agentFile != null) {
+    //    jvmArgs("-javaagent:${agentFile.absolutePath}")
+    //}
     jvmArgs("-XX:+EnableDynamicAgentLoading")
 
     testLogging {
